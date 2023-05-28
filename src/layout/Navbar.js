be31ba59d6f6c4cf481a372dff5969c8f55bb636
galrina <gala.purplesoft@gmail.com>
2023-05-28 21:35:17 +0300

    Routing

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
@@ -18,7 +19,9 @@ export default function Navbar() {
                         aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <button className="btn btn-outline-light">Add User</button>
+                    <Link className="btn btn-outline-light" to="/adduser">
+                        Add User
+                    </Link>
 
                 </div>
 
